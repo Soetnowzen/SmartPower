9b142ddf23a5fc716471fed6fb15dcc4476e616c
Soetnowzen <BaraFinkel@gmail.com>
2013-04-18 13:08:06 +0200

    Added so when you press the login button then the username and password is saved in a string veriable

--- src/com/sebbelebben/smartpower/LoginActivity.java
+++ src/com/sebbelebben/smartpower/LoginActivity.java
@@ -2,8 +2,12 @@ package com.sebbelebben.smartpower;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
@@ -12,6 +16,25 @@ public class LoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		Button loginbutton = (Button) findViewById(R.id.loginbutton);
+		
+		loginbutton.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				EditText UsernameBox = (EditText) findViewById(R.id.usernamebox);
+				EditText PasswordBox = (EditText) findViewById(R.id.passwordbox);
+				String Username = UsernameBox.getText().toString();
+				String Password = PasswordBox.getText().toString();
+				//Android#{"User":"<username","Password":"<password>"}:\n
+				//Android#{"User":Username,
+				//	"Password":Password}:\n
+			}
+			
+		});
+		/*aboutButton.setOnClickListener(new OnClickListener(){
+        	public void onClick(View arg0){
+        		Intent intent = new Intent(CounterActivity.this, AboutActivity.class);
+        		startActivity(intent);
+        	}
+        });*/
 	}
 
 	@Override
