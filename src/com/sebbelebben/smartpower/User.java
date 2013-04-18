35147ce4dd064048bc1832c70405496d89f5633f
bregell <johan_bregell@hotmail.com>
2013-04-18 16:43:12 +0200

    Added some getters to support the class

--- src/com/sebbelebben/smartpower/User.java
+++ src/com/sebbelebben/smartpower/User.java
@@ -1,13 +1,25 @@
 package com.sebbelebben.smartpower;
 
+import java.util.ArrayList;
 import org.json.*;
 
 public class User {
 	private String userName;
 	private String password;
-	boolean loggedIn = false;
+	private boolean loggedIn;
+	
+	public User(String userName, String password){
+		this.loggedIn = false;
+		this.userName = userName;
+		this.password = password;
+	}
+	
 	public String getUserName(){
 		return this.userName;
+	}
+	
+	public boolean loginStatus(){
+		return this.loggedIn;
 	}
 	
 	public void logIn(){
@@ -25,5 +37,23 @@ public class User {
 	
 	public void logOut(){
 		loggedIn = false;
+	}
+	
+	public PowerStrip[] getPowerStrips(){
+		ArrayList<PowerStrip> powerStripList = new ArrayList<PowerStrip>();
+		String result = Server.sendAndRecieve("{User:"+userName+",Request:powerstrips}");
+		try {
+			JSONObject data = new JSONObject(result);
+			if (data.getString("User") == userName){
+				JSONArray powerStrips = data.getJSONArray("powerStrips");
+				for(int i = 0; i < powerStrips.length(); i++){
+					JSONObject JSONpowerStrip = powerStrips.getJSONObject(i);
+					powerStripList.add(new PowerStrip(JSONpowerStrip.getInt("id"),JSONpowerStrip.getString("serialId"),1));
+				}
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return (PowerStrip[]) powerStripList.toArray();
 	}
 }
