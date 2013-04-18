117cef0fc6363ff1b95fe19840d801d0f917f53c
Soetnowzen <BaraFinkel@gmail.com>
2013-04-18 12:38:48 +0200

    Started working on the login screen

---
+++ src/com/sebbelebben/smartpower/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.sebbelebben.smartpower;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		Button loginbutton = (Button) findViewById(R.id.loginbutton);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
