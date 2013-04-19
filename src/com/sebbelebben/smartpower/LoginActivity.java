a7679f15d3515b9d471d91f0527374c817c19301
Soetnowzen <BaraFinkel@gmail.com>
2013-04-19 12:06:19 +0200

    Started working on the TCP connection

--- src/com/sebbelebben/smartpower/LoginActivity.java
+++ src/com/sebbelebben/smartpower/LoginActivity.java
@@ -3,29 +3,62 @@ package com.sebbelebben.smartpower;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class LoginActivity extends Activity {
-
+	private EditText mUsernameBox;
+	private EditText mPasswordBox;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		Button loginbutton = (Button) findViewById(R.id.loginbutton);
 		
-		loginbutton.setOnClickListener(new OnClickListener() {
+		//Finds the loginbutton on the login screen
+		Button loginButton = (Button) findViewById(R.id.loginbutton);
+		mUsernameBox = (EditText) findViewById(R.id.usernamebox);
+		mPasswordBox = (EditText) findViewById(R.id.passwordbox);
+		
+		
+		//Listens for when the button is pressed & takes the username & password...
+		loginButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
-				EditText UsernameBox = (EditText) findViewById(R.id.usernamebox);
-				EditText PasswordBox = (EditText) findViewById(R.id.passwordbox);
-				String Username = UsernameBox.getText().toString();
-				String Password = PasswordBox.getText().toString();
-				//Android#{"User":"<username","Password":"<password>"}:\n
-				//Android#{"User":Username,
-				//	"Password":Password}:\n
+				String username = mUsernameBox.getText().toString();
+				String password = mPasswordBox.getText().toString();
+				//Log.i("SmartPower",username +", "+password);
+				PrintWriter output = null;
+				BufferedReader input = null;
+				try {
+					Socket s = new Socket("localhost", 39500);
+					output = new PrintWriter(s.getOutputStream(),true);
+					input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+					output.println("Android#{User:"+username+", Password:"+password+"}:\n");
+					String inputMessage = input.readLine();
+					String tempUser = inputMessage.substring(14, username.length()+14);
+					Log.i("SmartPower",tempUser);
+					if(inputMessage.substring(14+username.length()).contains(",Login:true")) {
+						//startActivity(new Intent(this, MainActivity.class));
+					}
+				} catch(IOException error) {
+					error.toString();
+				} finally {
+					output.close();
+					//input.close();
+				}
+				//Android#{User:<username>,Password:<password>}:\n
+				//"Android#{User:"+Username+", Password:"+Password+"}:\n"
+				
+				//Android#{User:<username>,Login:<true/false>}
+				//*,Login:true*
 			}
 			
 		});
