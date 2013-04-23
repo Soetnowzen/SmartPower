7adfc732645edd7c29e601b278f3022214a5dd4c
Andreas Arvidsson <andreas.arvidson@gmail.com>
2013-04-23 18:01:13 +0200

    Renamed the fragments to their correct name.

--- src/com/sebbelebben/smartpower/fragments/ConsumptionFragment.java
+++ src/com/sebbelebben/smartpower/fragments/ConsumptionFragment.java
@@ -2,6 +2,6 @@ package com.sebbelebben.smartpower;
 
 import android.support.v4.app.Fragment;
 
-public class UserFragment extends Fragment {
+public class ConsumptionFragment extends Fragment {
 	
 }
