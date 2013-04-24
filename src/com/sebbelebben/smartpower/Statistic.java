8cbd7a15dd0c9f1cad53d7cd47a3edbeac4ace4c
henning <henningphan@hotmail.com>
2013-04-24 22:01:12 +0200

    cleared old code to make room for api branch

--- src/com/sebbelebben/smartpower/Statistic.java
+++ src/com/sebbelebben/smartpower/Statistic.java
@@ -1,10 +1,14 @@
 package com.sebbelebben.smartpower;
 
+//import android.R.string;
 import android.os.AsyncTask;
 
-import java.io.IOException;
-import java.net.Socket;
-import java.net.InetSocketAddress;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Statistic extends AsyncTask<String, Integer, Integer[]>{
 
@@ -13,19 +17,20 @@ public class Statistic extends AsyncTask<String, Integer, Integer[]>{
 	@Override
 	protected Integer[] doInBackground(String... params) {
 		// TODO Auto-generated method stub
-		InetSocketAddress ipep = new InetSocketAddress(params[0],Integer.parseInt(params[1]));
+
 		try{
-			Socket tcpSocket = new Socket(ipep.getAddress(), ipep.getPort());
-			String msg = "Android#{Starttime:" + params[2] +
-					",Endtime:" + params[3] + 
-					",\nSerialId:" + params[4] +
-					",SocketNumber/" + params[5] +
-					";SerialId:" + params[6] +
-					"/GroupId:" + params[7] + "}";
-			byte[] buffer = msg.getBytes("US-ASCII");
-			tcpSocket.getOutputStream().write(buffer);
-			
-		}catch(IOException e){
+			String msg = String.format("Android#{Starttime:%s" +
+			",Endtime:%s" + 
+			",SerialId:%s" + 
+			",SocketNumber/%s" + 
+			";SerialId:%s/GroupId:%s}", params[2],params[3],params[4], params[5],params[6],params[7]);
+			String reply = null;
+//			reply = user.sendandreceive(msg);
+			//assume has received
+			JSONObject js = new JSONObject(reply);
+			String data = js.getString("Data");
+			JSONArray ja = new JSONArray();
+		}catch(JSONException e){
 			
 		}
 		
