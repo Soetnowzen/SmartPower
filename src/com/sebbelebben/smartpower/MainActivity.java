bea2ab8abb933e54f47d7ae656be35ac8d978889
Andreas Arvidsson <andreas.arvidson@gmail.com>
2013-04-24 18:11:43 +0200

    Integrated the fragments in the MainActivity.

--- src/com/sebbelebben/smartpower/MainActivity.java
+++ src/com/sebbelebben/smartpower/MainActivity.java
@@ -3,14 +3,12 @@ package com.sebbelebben.smartpower;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.sebbelebben.smartpower.Server.OnLoginListener;
+import com.sebbelebben.smartpower.fragments.UserFragment;
 
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,95 +16,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
-	private EditText mUsernameBox;
-	private EditText mPasswordBox;
-	private User mUser;
-	
+public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-			
-		//Finds the loginbutton on the login screen
-		Button loginButton = (Button) findViewById(R.id.loginbutton);
-		mUsernameBox = (EditText) findViewById(R.id.usernamebox);
-		mPasswordBox = (EditText) findViewById(R.id.passwordbox);
-
-		//Checks the old preferences
-		loadPrefs();
-
-			
-		//Listens for when the button is pressed & takes the username & password...
-		loginButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View arg0) {
-				String username = mUsernameBox.getText().toString();
-				String password = mPasswordBox.getText().toString();
-				mUser = new User(username, password);
-				logIn();
-			}
-		});
-	}
-	
-	private void logIn() {
-		//Initiates the User with the new username & password
-		mUser.logIn(new OnLoginListener() {
-			@Override
-			public void onLoginSuccess() {
-				//Creates a JSONObject to save the user so the username & password will be needed to be 
-				//input every time
-				JSONObject jUser = new JSONObject();
-				try {
-					jUser.put("Username", mUser.getUserName());
-					jUser.put("Password", mUser.getPassword());
-					jUser.put("Logggedin", mUser.loginStatus());
-					savePrefs(jUser.toString());
-					Intent intent = new Intent(MainActivity.this, UserActivity.class);
-					intent.putExtra("User", mUser);
-					startActivity(intent);
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-			@Override
-			public void onLoginFailure() {
-				Toast.makeText(MainActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
-			}
-		});
-	}
-	
-	private void loadPrefs() {
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-		String user = sp.getString("USER",null);
-		try {
-			JSONObject jUser;
-			if(user == null) mUser = null;
-			else {
-				jUser = new JSONObject(user);
-				mUser = new User(jUser.getString("Username"),jUser.getString("Password"));
-				if(jUser.getBoolean("Loggedin")) {
-					logIn();
-				}
-			}
-		} catch (JSONException error) {
-			error.printStackTrace();
-		}
-		//If you're already logged in then skip this activity/screen
-		if(mUser != null && mUser.loginStatus()) {
-			startActivity(new Intent(this, UserActivity.class));
-		} else if(mUser != null){ //If the user ain't logged in then the password & username is entered into the boxes for them
-			mUsernameBox.setText(mUser.getUserName());
-			mPasswordBox.setText(mUser.getPassword());
-		}
-	}
-
-	private void savePrefs(String value) {
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-		Editor edit = sp.edit();
-		edit.putString("USER", value);
-		edit.commit();
+		Fragment f = UserFragment.newInstance();
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.add(R.id.container, f);
+		ft.commit();
 	}
 
 	@Override
--- src/com/sebbelebben/smartpower/fragments/ConsumptionFragment.java
+++ src/com/sebbelebben/smartpower/fragments/ConsumptionFragment.java
@@ -1,7 +1,25 @@
-package com.sebbelebben.smartpower;
+package com.sebbelebben.smartpower.fragments;
 
+import com.sebbelebben.smartpower.R;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ConsumptionFragment extends Fragment {
-	
+	public static ConsumptionFragment newInstance() {
+		ConsumptionFragment f = new ConsumptionFragment();
+		Bundle args = new Bundle();
+		f.setArguments(args);
+		return f;
+	}
+
+	public ConsumptionFragment() {
+	}
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_user, container, false);
+		return view;
+	}
 }
