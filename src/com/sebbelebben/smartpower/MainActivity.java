64771004647de583d3768bc1c88276352f052413
Andreas Arvidsson <andreas.arvidson@gmail.com>
2013-04-24 18:28:35 +0200

    Added a ViewPager to the MainActivity.

--- src/com/sebbelebben/smartpower/MainActivity.java
+++ src/com/sebbelebben/smartpower/MainActivity.java
@@ -1,25 +1,29 @@
 package com.sebbelebben.smartpower;
 
-import com.sebbelebben.smartpower.fragments.UserFragment;
+import com.sebbelebben.smartpower.fragments.*;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.ViewGroup;
 
 public class MainActivity extends FragmentActivity {
+	private ViewPager mPager;
+	private MainPagerAdapter mAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//ViewGroup container = (ViewGroup) findViewById(R.id.container);
-		Fragment f = UserFragment.newInstance();
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		ft.add(R.id.container, f);
-		ft.commit();
+
+		mPager = (ViewPager) findViewById(R.id.viewpager);
+		mAdapter = new MainPagerAdapter(getSupportFragmentManager());
+		mPager.setAdapter(mAdapter);
 	}
 
 	@Override
@@ -27,6 +31,38 @@ public class MainActivity extends FragmentActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private class MainPagerAdapter extends FragmentStatePagerAdapter {
+		private static final int COUNT = 3;
+
+		public MainPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			Fragment fragment = null;
+
+		    	switch(position) {
+			case 0:
+				fragment = RemoteFragment.newInstance();
+				break;
+			case 1:
+				fragment = UserFragment.newInstance();
+				break;
+			case 2:
+				fragment = ConsumptionFragment.newInstance();
+				break;
+			}
+			return fragment;
+		}
+
+		@Override
+		public int getCount() {
+			return COUNT;
+		}
+
 	}
 
 }
