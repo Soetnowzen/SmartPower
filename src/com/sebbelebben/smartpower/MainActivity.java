8fc87c3dac421a7ffc909431c06a2be13287dd11
Andreas Arvidsson <andreas.arvidson@gmail.com>
2013-04-24 18:11:43 +0200

    Integrated the fragments in the MainActivity.

--- src/com/sebbelebben/smartpower/MainActivity.java
+++ src/com/sebbelebben/smartpower/MainActivity.java
@@ -1,18 +1,25 @@
 package com.sebbelebben.smartpower;
 
+import com.sebbelebben.smartpower.fragments.UserFragment;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		startActivity(new Intent(this, LoginActivity.class));
+		//ViewGroup container = (ViewGroup) findViewById(R.id.container);
+		Fragment f = UserFragment.newInstance();
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.add(R.id.container, f);
+		ft.commit();
 	}
 
 	@Override
--- src/com/sebbelebben/smartpower/fragments/RemoteFragment.java
+++ src/com/sebbelebben/smartpower/fragments/RemoteFragment.java
@@ -1,7 +1,26 @@
-package com.sebbelebben.smartpower;
+package com.sebbelebben.smartpower.fragments;
 
+import com.sebbelebben.smartpower.R;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class RemoteFragment extends Fragment {
-	
+	public static RemoteFragment newInstance() {
+		RemoteFragment f = new RemoteFragment();
+		Bundle args = new Bundle();
+		f.setArguments(args);
+		return f;
+	}
+
+	public RemoteFragment() {
+	}
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_user, container, false);
+		return view;
+	}
 }
