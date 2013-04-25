7c48c3057a060811679fe4fe7cffff2f79b75933
Andreas Arvidsson <andreas.arvidson@gmail.com>
2013-04-25 20:55:33 +0200

    Started working on ListViews inside the fragments.

--- src/com/sebbelebben/smartpower/fragments/RemoteFragment.java
+++ src/com/sebbelebben/smartpower/fragments/RemoteFragment.java
@@ -1,5 +1,9 @@
 package com.sebbelebben.smartpower.fragments;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sebbelebben.smartpower.PowerStrip;
 import com.sebbelebben.smartpower.R;
 
 import android.os.Bundle;
@@ -7,8 +11,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class RemoteFragment extends Fragment {
+    	private ListView mListView;
+	private List<PowerStrip> mPowerStrips = new ArrayList<PowerStrip>();
+
 	public static RemoteFragment newInstance() {
 		RemoteFragment f = new RemoteFragment();
 		Bundle args = new Bundle();
@@ -19,8 +27,16 @@ public class RemoteFragment extends Fragment {
 	public RemoteFragment() {
 	}
 
+
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    mUser.getPowerStrips(new OnPowerStrip)
+
+	}
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_remote, container, false);
+		mListView = (ListView) view.findViewById(R.id.listview);
+		mAdapter = new ArrayAdapter<PowerStrip>(getActivity(), android.R.layout.simple_list_item_1, mPowerStrips);
 		return view;
 	}
 }
