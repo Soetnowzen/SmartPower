ff78655c9df005bcfe00317a6cd761d1b59e035a
henning <henningphan@hotmail.com>
2013-05-03 00:22:23 +0200

    fixed callback

--- src/com/sebbelebben/smartpower/Statistic.java
+++ src/com/sebbelebben/smartpower/Statistic.java
@@ -13,38 +13,47 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Statistic extends AsyncTask<String, Integer, Consumption[]>{
+import com.sebbelebben.smartpower.Server.OnConsumptionReceiveListener;
+import com.sebbelebben.smartpower.Server.OnReceiveListener;
+
+public class Statistic {
 
 	//make sure param[0] == ip, param[1] = port;
-	//all together its 7 params we need
-	@Override
-	protected Consumption[] doInBackground(String... params) {
+	public static void getStatistics(final OnConsumptionReceiveListener listener, String... params) {
 		// TODO Auto-generated method stub
 
-		try{
-			String msg = String.format("Android#{Starttime:%s" +
-			",Endtime:%s" + 
-			",SerialId:%s" + 
-			",SocketNumber/%s" + 
-			";SerialId:%s/GroupId:%s}", params[2],params[3],params[4], params[5],params[6],params[7]);
-			String reply = Server.sendAndRecieve(msg);
+		String msg = String.format("Android#{Starttime:%s" +
+				",Endtime:%s" + 
+				",SerialId:%s" + 
+				",SocketNumber/%s" + 
+				";SerialId:%s/GroupId:%s}", params[2],params[3],params[4], params[5],params[6],params[7]);
+		Server.sendAndRecieve(msg, new OnReceiveListener() {
+			@Override
+			public void onReceive(String result) {
+				JSONObject js;
+				try {
+					js = new JSONObject(result);
 
-			JSONObject js = new JSONObject(reply);
-			JSONArray dataArray = js.getJSONArray("data");
-			Consumption[] result = new Consumption[dataArray.length()];
-			for(int i = 0; i < dataArray.length(); i++){
-				JSONObject jo = dataArray.getJSONObject(i);
-				int power = jo.getInt("power");
-				String time = jo.getString("time");
-				result[i] = new Consumption(time,power);
+					JSONArray dataArray = js.getJSONArray("data");
+					Consumption[] reply = new Consumption[dataArray.length()];
+					for(int i = 0; i < dataArray.length(); i++){
+						JSONObject jo = dataArray.getJSONObject(i);
+						int power = jo.getInt("power");
+						String time = jo.getString("time");
+						reply[i] = new Consumption(time,power);
+					}
+					listener.onConsumptionReceive(reply);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					listener.failed();
+				}
+
 			}
-			return result;			
-			
-		}catch(JSONException e){
-			return null;
-		}		
+		});
+
 	}
-	
-	
+
+
 
 }
