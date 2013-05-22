3c789425d64f1ef7a15fa25204e1e84d4b99e5a4
Johan Swetzén <johan@swetzen.com>
2013-05-22 14:40:09 +0200

    Adds test for rotating screen, logout and remember credentials

--- src/com/sebbelebben/smartpower/tests/LoginActivityTest.java
+++ src/com/sebbelebben/smartpower/tests/LoginActivityTest.java
@@ -31,6 +31,7 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
 		super.setUp();
 	}
 	
+	// Does log in work?
 	public void testLogin() throws Exception {
 		solo.assertCurrentActivity("wrong activity", LoginActivity.class);
 		
@@ -51,6 +52,62 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
 		solo.assertCurrentActivity("wrong activity", LoginActivity.class);
 		
 		solo.waitForActivity("MainActivity");
+	}
+	
+	// Does the log out function actually log the user out?
+	// User story 48567259
+	public void testLogout() throws Exception {
+		solo.assertCurrentActivity("wrong activity", LoginActivity.class);
+		
+		// No autologin should occur with cleared preferences
+		View v = solo.getView(com.sebbelebben.smartpower.R.id.loading_progress); 
+		assertFalse(v.getVisibility() == View.VISIBLE);
+		
+		// Enter login details for test account
+		solo.clearEditText(0);
+		solo.enterText(0, "android");
+		solo.clearEditText(1);
+		solo.enterText(1, "android");
+		solo.clickOnImageButton(0);
+		
+		solo.waitForActivity("MainActivity");
+		
+		getActivity().startActivity(new Intent(getActivity(), LoginActivity.class));
+		solo.assertCurrentActivity("wrong activity", LoginActivity.class);
+		
+		solo.waitForActivity("MainActivity");
+		
+		solo.sendKey(Solo.MENU);
+		solo.clickOnText("Log out");
+		
+		solo.waitForText("Username", 1, 5000); // This text found => username cleared
+	}
+	
+	// Does the automatic login work after closing the app and reopening it?
+	// Part of user story 46902313 (credentials remembered)
+	public void testAutomaticLogin() throws Exception {
+		solo.assertCurrentActivity("wrong activity", LoginActivity.class);
+		
+		// No autologin should occur with cleared preferences
+		View v = solo.getView(com.sebbelebben.smartpower.R.id.loading_progress); 
+		assertFalse(v.getVisibility() == View.VISIBLE);
+		
+		// Enter login details for test account
+		solo.clearEditText(0);
+		solo.enterText(0, "android");
+		solo.clearEditText(1);
+		solo.enterText(1, "android");
+		solo.clickOnImageButton(0);
+		
+		solo.waitForActivity("MainActivity", 5000);
+		
+		// Close app and reopen
+		solo.finishOpenedActivities();
+		solo.sleep(1000);
+		this.launchActivity("com.sebbelebben.smartpower", LoginActivity.class, null);
+		solo.sleep(1000);
+		solo.waitForActivity("MainActivity", 5000);
+		
 	}
 	
 	@Override
--- src/com/sebbelebben/smartpower/tests/MainActivityTest.java
+++ src/com/sebbelebben/smartpower/tests/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.sebbelebben.smartpower.tests;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import com.jayway.android.robotium.solo.Solo;
@@ -52,7 +53,10 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<LoginActi
 		solo.waitForActivity("MainActivity");
 		
 		// Rename an outlet
-		solo.clickInList(0);
+		
+		ArrayList<android.widget.TextView> listTexts = solo.clickInList(0);
+		String oldName = (String) listTexts.get(0).getText();
+		
 		solo.clickOnButton("Rename");
 		double rand = Math.random() * 10000;
 		
@@ -62,10 +66,42 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<LoginActi
 		// Make sure the item is renamed
 		solo.waitForText("robotiumTest" + rand, 1, 5000);
 		solo.getText("robotiumTest" + rand);
+		
+		// Rename the outlet back to what it was
+		solo.clickOnButton("Rename");
+		solo.enterText(0, oldName);
+		solo.clickOnButton("OK");
+		
+	}
+	
+	public void testRotateScreen() throws Exception {
+		// Rotate to landscape
+		solo.setActivityOrientation(Solo.LANDSCAPE);
+		
+		solo.assertCurrentActivity("wrong activity", LoginActivity.class);
+		
+		// No autologin should occur with cleared preferences
+		View v = solo.getView(com.sebbelebben.smartpower.R.id.loading_progress); 
+		assertFalse(v.getVisibility() == View.VISIBLE);
+		
+		// Enter login details for test account
+		solo.clearEditText(0);
+		solo.enterText(0, "android");
+		solo.clearEditText(1);
+		solo.enterText(1, "android");
+		solo.clickOnImageButton(0);
+		
+		solo.waitForActivity("MainActivity");
+		
+		// Rotate back to portrait after logging in
+		solo.setActivityOrientation(Solo.PORTRAIT);
+		solo.clickInList(0);
+		solo.waitForText("Button", 1, 5000, true);
 	}
 	
 	@Override
 	public void tearDown() throws Exception {
 		solo.finishOpenedActivities();
+		
 	}
 }
