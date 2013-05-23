0bf0acd07744a96c9c733d466d7bc5f87fce40f7
henning <phanhenning@gmail.com>
2013-05-23 22:04:30 +0200

    cleaning code in GraphActivity

--- src/com/sebbelebben/smartpower/GraphActivity.java
+++ src/com/sebbelebben/smartpower/GraphActivity.java
@@ -37,93 +37,43 @@ public class GraphActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_graph);
 		final GraphView graphView = (GraphView) findViewById(R.id.graphview);
+		final ProgressBar pb = (ProgressBar) findViewById(R.id.loading_progress);
 		Intent intent = getIntent();
 		final PowerStrip mPowerStrip = (PowerStrip) intent.getSerializableExtra("PowerStrip");
-
-
-		/*
-		List<Point> data = new ArrayList<Point>();
-		// TODO: Use real implementation - this is only test data
-		data.add(new Point(-3.0f, 3.0f));
-		data.add(new Point(0.0f, 0.0f));
-		data.add(new Point(1.0f, 1.0f));
-		data.add(new Point(2.0f, 0.0f));
-		data.add(new Point(3.0f, 1.0f));
-		data.add(new Point(4.0f, 0.0f));
-		data.add(new Point(5.0f, 1.0f));
-		data.add(new Point(6.0f, 0.0f));
-		data.add(new Point(7.5f, 3.0f));
-		 */
-
-		final AlertDialog.Builder alert = new AlertDialog.Builder(this);
-		LayoutInflater factory = LayoutInflater.from(this);
-		final View input = factory.inflate(R.layout.doubleinput_remote, null);
 		final Context context = this;
-		final List<Consumption> data = new ArrayList<Consumption>();
-//		DatePickerDialog dpg = new DatePickerDialog(this, null, 1931, 03, 24);
-//		dpg.show();
-		//final DatePicker dp = (DatePicker) findViewById(R.id.datePicker1); //new DatePicker(context);
-		alert.setView(input);
+		Calendar cal = Calendar.getInstance();
 		
-
-		alert.setPositiveButton("Send", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
+		final List<Consumption> data = new ArrayList<Consumption>();
+		
+		DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+			
+			@Override
+			public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZZ", Locale.ENGLISH);
-				Calendar cal = Calendar.getInstance();
-
-				DatePicker dp = (DatePicker) input.findViewById(R.id.datePicker1);
-				final ProgressBar pb = (ProgressBar) findViewById(R.id.loading_progress);
-				pb.setVisibility(ProgressBar.VISIBLE);
-				
-				int day = dp.getDayOfMonth();
-				int year = dp.getYear();
-				int month = dp.getMonth();
-
-				String start = String.format("%s-%s-%s 00:00:00+00",year,month,day);
-				
-				
+				String start = String.format("%s-%s-%s 00:00:00+00",year,monthOfYear,dayOfMonth);
 				try{
 					mPowerStrip.getConsumption(sdf.parse(start), new Date(System.currentTimeMillis()), new OnConsumptionReceiveListener() {
 
 						@Override
 						public void onConsumptionReceive(Consumption[] consumption) {
-							Toast.makeText(context,"OnConsumptionReceiveListener", Toast.LENGTH_SHORT).show();
 							Collections.addAll(data, consumption);
-							
 							display(data, graphView);
 							pb.setVisibility(ProgressBar.GONE);
-							
 						}
 
 						@Override
 						public void failed() {
 							Toast.makeText(context, "FAILED TO GET CONSUMPTION", Toast.LENGTH_SHORT).show();
-							pb.setVisibility(ProgressBar.GONE);
 						}
 					});
 				}catch(ParseException e){
 					Log.d("bug", "Error with parsing: " + e.getStackTrace());
 				}
+				
 			}
-		});
-
-		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-
-			public void onClick(DialogInterface dialog, int whichButton) {
-				// Canceled.
-			}
-
-		});
-		alert.show();
-
-		/*
-		data.add(new Consumption("2010-05-23 12:05:13+02", 100));
-		data.add(new Consumption("2010-05-24 12:05:13+02", 123));
-		data.add(new Consumption("2010-05-25 12:05:13+02", 65));
-		data.add(new Consumption("2010-05-26 12:05:13+02", 68));
-		data.add(new Consumption("2010-05-27 12:05:13+02", 100));
-		//Todo: sort data
-		 */
+		};
+		DatePickerDialog dp = new DatePickerDialog(this, mDateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH));
+		dp.show();
 
 	}
 	private void display(List<Consumption> data, GraphView graphView){
