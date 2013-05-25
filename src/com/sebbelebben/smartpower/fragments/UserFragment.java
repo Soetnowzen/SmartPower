c606a6507ee9e2cec4711399f752cb91abcf4cd9
henning <phanhenning@gmail.com>
2013-05-25 12:39:50 +0200

    changed userfragment and graphactivity takes graphable

--- src/com/sebbelebben/smartpower/fragments/UserFragment.java
+++ src/com/sebbelebben/smartpower/fragments/UserFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView.FindListener;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class UserFragment extends SherlockFragment {
 	public static UserFragment newInstance(User user) {
@@ -22,8 +23,12 @@ public class UserFragment extends SherlockFragment {
 	}
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		final User user = (User) getArguments().getSerializable("User");
 		View view = inflater.inflate(R.layout.fragment_user, container, false);
 		view.findViewById(R.id.loading_progress).setVisibility(ProgressBar.GONE);
+		String str = String.format("name: %s\npwd: %s", user.getUserName(), user.getPassword());
+		((TextView) view.findViewById(R.id.textView)).setText(str);
+		
 		return view;
 	}
 }
