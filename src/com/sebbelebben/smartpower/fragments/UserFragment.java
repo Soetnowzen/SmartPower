e848769e06baa933d6d5ee78ba1e9ef719076d54
henning <phanhenning@gmail.com>
2013-05-25 21:00:53 +0200

    PsSocketAdapter created

--- src/com/sebbelebben/smartpower/fragments/UserFragment.java
+++ src/com/sebbelebben/smartpower/fragments/UserFragment.java
@@ -1,16 +1,28 @@
 package com.sebbelebben.smartpower.fragments;
+import java.util.ArrayList;
+
+import com.sebbelebben.smartpower.PsSocket;
 import com.sebbelebben.smartpower.User;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.sebbelebben.smartpower.R;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView.FindListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class UserFragment extends SherlockFragment {
+	ArrayList<PsSocket> list;
+	SocketAdapter mAdapter;
+	ListView listView;
+	
 	public static UserFragment newInstance(User user) {
 		UserFragment f = new UserFragment();
 		Bundle args = new Bundle();
@@ -30,5 +42,27 @@ public class UserFragment extends SherlockFragment {
 		((TextView) view.findViewById(R.id.textView)).setText(str);
 		
 		return view;
+	}
+	public class SocketAdapter extends ArrayAdapter<PsSocket>{
+		private ArrayList<PsSocket> objects;
+		
+		public SocketAdapter(Context context, int textViewResourceId, ArrayList<PsSocket> objects){
+			super(context, textViewResourceId, objects);
+			this.objects = objects;
+		}
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			if( v == null)	{
+				LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				v = inflater.inflate(R.layout.powerstrip_item, null);
+			}
+			PsSocket socket = objects.get(position);
+			TextView tv = (TextView) v.findViewById(R.id.text);
+			ToggleButton tb = (ToggleButton) v.findViewById(R.id.toggle_button);
+			if ( tv != null) tv.setText(socket.getName());
+			if (tb != null) tb.setChecked(true);
+			return v;
+		}
+	
 	}
 }
