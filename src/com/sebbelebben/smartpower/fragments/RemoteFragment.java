8bbd98c16d1804c5d80196157468b30ae769391d
Johan Swetzén <johan@swetzen.com>
2013-05-28 09:28:14 +0200

    Fixes Remotefragment after begin reset to non working state

--- src/com/sebbelebben/smartpower/fragments/RemoteFragment.java
+++ src/com/sebbelebben/smartpower/fragments/RemoteFragment.java
@@ -149,38 +149,8 @@ public class RemoteFragment extends SherlockFragment {
             ImageButton renameButton = (ImageButton) view.findViewById(R.id.rename_btn);
             final ImageButton favoriteButton = (ImageButton) view.findViewById(R.id.favorite_btn);
             ImageButton consumptionButton = (ImageButton) view.findViewById(R.id.consumption_btn);
-            toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        child.turnOn(new GenericListener() {
-                            @Override
-                            public void success() {
-                                toggleButton.setChecked(true);
-                            }
 
-                            @Override
-                            public void failed() {
-                                toggleButton.setChecked(false);
-                                Toast.makeText(getActivity(), getResources().getString(R.string.turn_on_failure), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } else {
-                        child.turnOff(new GenericListener() {
-                            @Override
-                            public void success() {
-                                toggleButton.setChecked(false);
-                            }
-
-                            @Override
-                            public void failed() {
-                                toggleButton.setChecked(true);
-                                Toast.makeText(getActivity(), getResources().getString(R.string.turn_off_failure), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                }
-            });
+            toggleButton.setChecked(child.getStatus());
 
             toggleButton.setOnClickListener(new OnClickListener() {
 				
